9f4cec843e29215adf9de3963120363e796cda00
beyza6565 <Beyzanur.Yilmaz@Student.HTW-Berlin.de>
2026-04-17 08:52:24 +0200

    initial commint

---
+++ src/main/java/htw/webtech/dailyhabits/DailyhabitsApplication.java
@@ -0,0 +1,13 @@
+package htw.webtech.dailyhabits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyhabitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyhabitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw/webtech/dailyhabits/DailyhabitsApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.webtech.dailyhabits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyhabitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
